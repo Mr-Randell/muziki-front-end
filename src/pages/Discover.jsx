50e2b3689f5d32a8ae6d9b99f39043697362f9ee
AlexKarumbi <alex.muhia@moringaschool.com>
2022-12-07 15:00:30 +0300

    Add songcards to discover

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,6 +6,17 @@ import { genres } from "../assets/constants";
 const Discover = () => {
   return (
     <div className="flex flex-col">
+
+    <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+      {[1,2,3,4,5,6,7,9,10].map((song,i) =>(
+        <SongCard
+        key ={song.key}
+        song={song}
+        i={i}
+        />
+      ))}
+
+    </div>
       
     </div>
   )
