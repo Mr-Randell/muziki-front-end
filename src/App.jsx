6fb8dea96c9442d2e5a135d04cda9e20efc11c87
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-12-07 11:34:20 +0300

    Feat: updated the base app visual layout

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+// import logo from './logo.svg';
+// import './App.css';
+import { useSelector } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import { SearchBar, SideBar, MusicPlayer, TopPlay } from "./components";
+import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from "./pages";
+
+function App() {
+  // const { activeSong } = useSelector((state) => state.player)
+  return (
+    <div className="relative flex">
+      <SideBar />
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        <SearchBar />
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+            <Routes>
+              <Route path="/" element={ <Discover/>} />
+              <Route path="/top-artists" element={ <TopArtists />} />
+              <Route path="/top-charts" element={ <TopCharts />} />
+              <Route path="/around-you" element={ <AroundYou />} />
+              <Route path="/artists/:id" element={ <ArtistDetails />} />
+              <Route path="/songs/:song_id" element={ <SongDetails />} />
+              <Route path="/search/search_term" element={ <Search />} />
+            </Routes>
+          </div>
+          <div className="xl:sticky relative top-0 h-fit">
+            <TopPlay />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
