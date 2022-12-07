cbdddda55001df0c44f9a293c64f7e1a903718d5
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-12-07 17:35:53 +0300

    latest code for discover

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,6 +6,7 @@ import { genres } from "../assets/constants";
 const Discover = () => {
     const genreTitle = "Pop";
   return (
+    // latest code content for Discover
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">
