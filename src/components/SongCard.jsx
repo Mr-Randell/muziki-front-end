b697f4401a29638cc96b9e2cc7774f79f8600c9e
AlexKarumbi <alex.muhia@moringaschool.com>
2022-12-07 15:43:13 +0300

    Add songcard pause and play functions

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,29 +1,46 @@
 import React from 'react'
-import {Link } from 'react-redux'
+import {Link } from 'react-router-dom'
+import { dispatch } from 'rxjs/internal/observable/pairs';
 
 import {playerSlice,setActiveSong} from '../redux/features/playerSlice'
 import PlayPause from './PlayPause';
 
-const SongCard = ({song,i}) => {
-  const activeSong = "Test"
+const SongCard = ({song,i,isPlaying,activeSong,data}) => {
+  
+  const activeSong = "Test";
+  const handlePauseClick=()=>{
+    dispatch (playPause(false));
+
+  };
+  const handlePlayClick=()=>{
+    dispatch (setActiveSong({sonf,data,i}));
+    dispatch(playPause(true));
+    
+  };
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex 
       ${activeSong?.title === song.title ? "flex bg-black bg-opacity-70":"hidden" }`}>
-      <PlayPause/>
+      <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause ={handlePauseClick}
+      handlePlay ={handlePlayClick}
+      />
       
         </div>
         <img alt ="song_img" src=""/>
       </div>
       <div className="mt-4 flex flex-col">
         <p className="font-semibold text-lg text-white truncate">
-          <Link to ={`/songs/${song?.key}`}>
+          <Link to={`/songs/${song?.key}`}>
           {song.title}
           </Link>
         </p>
         <p className="text-sm truncate text-gray-300 mt-1">
-          <Link to ={song.artist?`/artist/${song?.artists[0]?.adamid}`:"/top-artists"}>
+          <Link to={song.artist?`/artist/${song?.artists[0]?.adamid}`:"/top-artists"}>
           {song.subtitle}
           </Link>
         </p>
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -4,15 +4,20 @@ import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 
 const Discover = () => {
+  const dispatch=useDispatch();
+  const{ activeSong,isPlaying } =useSelector((state)=>state.player);
   return (
     <div className="flex flex-col">
 
     <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-      {[1,2,3,4,5,6,7,9,10].map((song,i) =>(
+      {[1,2,3,4,5,6,7,9,10].map((song,i ) =>(
         <SongCard
         key ={song.key}
         song={song}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
         i={i}
+        data ={data}
         />
       ))}
 
