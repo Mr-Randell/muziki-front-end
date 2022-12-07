961683d5a93a6e870962e5c78cdb162728caafb7
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-12-08 02:52:22 +0300

    Feat: added dynamic sidebar component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+// import { logo } from "../assets";
+import { links } from "../assets/constants";
+import { HiOutlineMenu } from 'react-icons/hi';
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    { links.map(( item ) => ( 
+      <NavLink 
+      key={ item.name } 
+      to={ item.to } className="flex flex-row justify-start items-center my-10 text-sm font-medium text-gray-400 hover:text-cyan-400"
+      onClick={ () => handleClick && handleClick() }
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        { item.name }
+      </NavLink>
+     )) }
+  </div>
+);
 
 const SideBar = () => {
+  const [ mobileMenuOpen, setMobileMenuOpen ] = useState(false);
   return (
-    <div>
-      SideBar
-    </div>
+    <>
+      {/* desktop menu */}
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        {/* add logo below here once svg is available in assets dir */}
+        {/* <img src={logo} alt="logo" className="w-full h-14 object-contain" /> */}
+        <NavLinks />
+      </div>
+
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? ( 
+          <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={ () => setMobileMenuOpen( false ) } />
+        ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={ () => setMobileMenuOpen( true ) } />}
+      </div>
+
+      {/* mobile menu */}
+      <div className={ `absolute top-0 h-screen w-2/3 bg-gradient-2-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}` }>
+        {/* <img src={logo} alt="logo" className="w-full h-14 object-contain" /> */}
+        <NavLinks handleClick={ () => setMobileMenuOpen( false ) } />
+      </div>
+    </>
   )
 }
 
