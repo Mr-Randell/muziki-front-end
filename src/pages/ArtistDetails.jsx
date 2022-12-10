abfaa16ff0a393c02201adc53a52b00af8cd6bd9
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-12-10 17:44:31 +0300

    Feat: added artistDetails feature

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
+
 
 const ArtistDetails = () => {
+  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery( artistId );
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector( (state) => state.player );
+
+  if( isFetchingArtistDetails ) return <Loader title= "Loading artist details..." />;
+  if( error ) return <Error />;
+
   return (
-    <div>
-      ArtistDetails
+    <div className="flex flex-col">
+      <DetailsHeader 
+        artistId = { artistId }
+        artistData = { artistData }
+      />
+      
+      <RelatedSongs 
+        data = { Object.values( artistData?.songs ) }
+        artistId = { artistId }
+        activeSong = { activeSong }
+        isPlaying = { isPlaying }
+      />
     </div>
   )
 }
