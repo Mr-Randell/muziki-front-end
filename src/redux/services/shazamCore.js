6be87f6a8027abf50f00cb152ac51fafaaccc93d
AlexKarumbi <alex.muhia@moringaschool.com>
2022-12-11 06:37:18 +0300

    Add around you logic

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,38 +1,24 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const options = {  
-//     method: "GET",
-//     headers: {
-//         "Content-Type": "Application/json"
-//     }
-// };
-
-// fetch("http://localhost:3000/genre/:1/songs", options)
-// .then(( response ) => response.json())
-// .then(( response ) => console.log(response))
-// .catch(( err ) => console.log(err));
-
 export const shazamCoreApi = createApi({ 
     reducerPath: "shazamCoreApi",
     baseQuery: fetchBaseQuery({     
         baseUrl: "http://localhost:3000",
-        // baseUrl: "https://shazam-core.p.rapidapi.com/v1",
         prepareHeaders: (headers) => {
             headers.set( "Content-Type", "application/json" );
-            // headers.set( "X-RapidAPI-Key", "KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA" );
             return headers;
         },
     }),
     endpoints: ( builder ) => ({ 
         // all endpoints go here
         getSongs: builder.query({ query: () => "/songs" }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
         getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
-        // getTopCharts: builder.query({ query: () => "/charts/world" }),
     }),
 });
 
 export const { 
     useGetSongsQuery,
+    useGetSongsByCountryQuery,
     useGetSongsBySearchQuery,
-    // useGetTopChartsQuery,
 } = shazamCoreApi;
